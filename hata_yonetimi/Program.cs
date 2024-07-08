fe713eabbc81a05f3b04b013946e6d84e28f16e2
Halil Özcan <131258476+Halil-x27@users.noreply.github.com>
2024-07-08 12:43:39 +0300

    Add files via upload

---
+++ hata_yonetimi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("bir sayi giriniz");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("girmiş olduğunuz sayi : " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("hata :" + ex.Message.ToString());
+            }
+            finally{
+                Console.WriteLine("işlem tamamlandı.");
+            }
+
+
+            try
+            {
+                int a = int.Parse(null);
+
+                int b = int.Parse("test");
+            }
+            catch (ArgumentNullException ex)
+            {
+                 Console.WriteLine("Bos deger girdiniz");
+                 Console.WriteLine(ex);
+            }
+            catch (FormatException ex){
+                 Console.WriteLine("Girilen değer tipi uygun değil....");
+                 Console.WriteLine(ex);
+            }
+            }
+        }
+    }
+}
